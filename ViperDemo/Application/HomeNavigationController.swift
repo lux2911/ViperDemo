f5f8a595bfa9986594dbd52c16f02858e987fc18
lux <lookateach1@gmail.com>
2019-09-23 09:56:49 +0100

    First

---
+++ ViperDemo/Application/HomeNavigationController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeNavigationController.swift
+//  ViperDemo
+//
+//  Created by Tomislav Luketic on 9/22/19.
+//  Copyright © 2019 lux. All rights reserved.
+//
+
+import UIKit
+
+class HomeNavigationController: UINavigationController {
+
+}
